1cbaa89bc3e16490b0bd8e56e2e9b50df1e58616
Emran <emran.bm@gmail.com>
2016-12-25 21:35:07 +0330

    New games section

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,53 @@
  * Created by emran on 12/24/16.
  */
 $(document).ready(function () {
-    let carousel = $('.owl-carousel');
-    carousel.owlCarousel({
-        loop:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true,
-                dots:false
+    let mainCarousel = $('#main-carousel');
+    mainCarousel.owlCarousel({
+        loop: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                dots: false
             },
-            600:{
-                items:3,
-                nav:false,
-                dots:false
+            600: {
+                items: 3,
+                nav: false,
+                dots: false
             },
-            1000:{
-                items:6,
-                loop:true,
-                dots:false
+            1000: {
+                items: 6,
+                loop: true,
+                dots: false
             }
         }
     });
+
+    let newGamesCarousel = $('#new-games-carousel');
+    newGamesCarousel.owlCarousel({
+        responsiveClass: true,
+        margin: 15,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                dots: true
+            },
+            600: {
+                items: 2,
+                nav: true,
+                dots: true
+            },
+            1000: {
+                items: 4,
+                loop: false,
+                dots: true
+            }
+        }
+    });
+
+    // $.get("http://api.ie.ce-it.ir/F95/home", function(data, status){
+    //     alert("Data: " + data + "\nStatus: " + status);
+    // });
 });
