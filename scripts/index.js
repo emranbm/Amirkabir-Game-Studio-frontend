c714d4aefe00d4210ae5019a723af9da4785c1dc
Emran <emran.bm@gmail.com>
2016-12-27 18:52:57 +0330

    footer refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,35 @@
 /**
  * Created by emran on 12/24/16.
  */
-$(document).ready(function () {
+$.get("F95/home", function (data, status) {
+    if (status !== 'success') {
+        alert('مشکل در ارتباط با سرور');
+        return;
+    }
 
-    $.get("F95/home", function (data, status) {
-        if (status !== 'success') {
-            alert('مشکل در ارتباط با سرور');
-            return;
-        }
+    let response = JSON.parse(data).response;
 
-        let response = JSON.parse(data).response;
+    if (!response.ok) {
+        alert('مشکل در بارگذازی صفحه');
+        return;
+    }
 
-        if (!response.ok) {
-            alert('مشکل در بارگذازی صفحه');
-            return;
-        }
+    let homepage = response.result.homepage;
 
-        let homepage = response.result.homepage;
+    // Main carousel
+    for (let slide of homepage.slider) {
+        let item = new MainCarouselItem(slide.title, slide.small_image);
+        $("#main-carousel").append(item);
+    }
 
-        // Main carousel
-        for (let slide of homepage.slider) {
-            let item = new MainCarouselItem(slide.title, slide.small_image);
-            $("#main-carousel").append(item);
-        }
+    // New games carousel
+    for (let newGame of homepage.new_games) {
+        let item = new NewGameCarouselItem(newGame.title, newGame.categories.join('، '), Math.floor(newGame.rate), newGame.small_image);
+        $("#new-games-carousel").append(item);
+    }
 
-        // New games carousel
-        for (let newGame of homepage.new_games) {
-            let item = new NewGameCarouselItem(newGame.title, newGame.categories.join('، '), Math.floor(newGame.rate), newGame.small_image);
-            $("#new-games-carousel").append(item);
-        }
+    startCarousels();
 
-        startCarousels();
-
-        // Comments
-        for (let comment of homepage.comments) {
-            let item = new CommentItem(comment.text, comment.date, comment.player.avatar);
-            $("#comments-div").append(item);
-        }
-
-        // Tutorials
-        for (let tut of homepage.tutorials) {
-            let item = new TutorialItem(tut.title, tut.date, tut.game.small_image);
-            $("#tutorials-div").append(item);
-        }
-
-    });
 });
 
 ////
@@ -182,78 +167,6 @@ function startCarousels() {
             }
         }
     });
-}
-
-/**
- * <div class="row item">
- <div class="col-md-10">
- <span class="text-info small-text">بهترین بازی تمام عمرم</span><br/>
- <h6 class="text-muted">دوشنبه ۱۷ آبان ۹۵</h6>
- </div>
- <div class="col-md-2">
- <img class="round-img" src="images/421645.jpg"/>
- </div>
- </div>
- * @param text
- * @param date
- * @constructor
- */
-function CommentItem(text, date, avatarUrl) {
-    let item = emranHelper.newDiv('row item');
-
-    let div1 = emranHelper.newDiv('col-md-10');
-    item.appendChild(div1);
-    let span = emranHelper.newElement('span', 'text-info small-text');
-    span.innerHTML = text;
-    div1.appendChild(span);
-    let h6 = emranHelper.newElement('h6', 'text-muted');
-    h6.innerHTML = date;
-    div1.appendChild(h6);
-
-    let div2 = emranHelper.newDiv('col-md-2');
-    item.appendChild(div2);
-    let img = emranHelper.newElement('img', 'round-img');
-    img.setAttribute('src', avatarUrl);
-    div2.appendChild(img);
-
-    return item;
-}
-
-/**
- * <div class="row item">
- <div class="col-md-10">
- <span class="text-info small-text">بهترین بازی تمام عمرم</span><br/>
- <h6 class="text-muted">دوشنبه ۱۷ آبان ۹۵</h6>
- </div>
- <div class="col-md-2">
- <img src="images/421645.jpg"/>
- </div>
- </div>
- * @param text
- * @param date
- * @param avatarUrl
- * @return {Element}
- * @constructor
- */
-function TutorialItem(text, date, avatarUrl) {
-    let item = emranHelper.newDiv('row item');
-
-    let div1 = emranHelper.newDiv('col-md-10');
-    item.appendChild(div1);
-    let span = emranHelper.newElement('span', 'text-info small-text');
-    span.innerHTML = text;
-    div1.appendChild(span);
-    let h6 = emranHelper.newElement('h6', 'text-muted');
-    h6.innerHTML = date;
-    div1.appendChild(h6);
-
-    let div2 = emranHelper.newDiv('col-md-2');
-    item.appendChild(div2);
-    let img = emranHelper.newElement('img');
-    img.setAttribute('src', avatarUrl);
-    div2.appendChild(img);
-
-    return item;
 }
 
 
